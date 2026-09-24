e9e151d1776ed0a3764cd98406f9f7f23e2b7b26
Gourav <70527839+gourav789@users.noreply.github.com>
2026-09-24 12:56:49 +0530

    Add Meta Pixel for Facebook ads tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,12 @@
 import "./globals.css";
+import Script from "next/script";
 import { site } from "@/lib/site";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { AuthProvider } from "@/components/AuthProvider";
+
+// Meta (Facebook) Pixel ID
+const META_PIXEL_ID = "2376608636415859";
 
 export const dynamic = "force-dynamic";
 
@@ -25,6 +29,32 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
         <AuthProvider>
           <Navbar />
           <main className="min-h-[70vh]">{children}</main>
